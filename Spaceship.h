c0bcff648cb88f4ad295d8be078047573880461c
Mariana-s-h <mariana.silva.honorato@gmail.com>
2026-02-19 18:29:51 +0000

    Add files via upload
    background and spaceship animations

---
+++ Spaceship.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Mechanism/Pawn.h"
+#include <functional>
+
+class Spaceship : public Mechanism::Pawn
+{
+public:
+
+	Spaceship(void* renderer, const char* texturePath, float x, float y, int gridColumns, int gridRows, int frameIndex = 0);
+
+	~Spaceship();
+
+	void PlayerUpdate(float deltaTime);
+
+	void Shoot();
+
+	void SetShootCallback(const std::function<void(float, float)>& callback)
+	{
+		m_ShootCallback = callback;
+	}
+
+private:
+
+	float m_ShootCooldown;
+	float m_ShootCooldownTime;
+	std::function<void(float, float)> m_ShootCallback;
+
+	int   m_CurrentFrame = 3;
+};
+
